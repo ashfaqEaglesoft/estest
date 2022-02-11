dc8309828595f8e0d2004bbdfd587d4f2a55e88a
ashfaqEaglesoft <yahooashfaq@gmail.com>
2022-02-11 11:24:32 +0500

    first commit

---
+++ main/urls.py
@@ -0,0 +1,10 @@
+
+from unicodedata import name
+from django.contrib import admin
+from django.urls import path
+from django.urls import include
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='home')
+]
